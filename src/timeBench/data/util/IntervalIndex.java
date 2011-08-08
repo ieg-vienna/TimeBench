13df14107f3cb10755937bb2316f5eaa569b40a2
Bilal Alsallakh <bilalsal@gmail.com>
2011-08-08 14:31:59 +0000

    1. add BipartiteGraph 2. add interval search 3. add test classes 4. add dependency on Prefuse

---
+++ src/timeBench/data/util/IntervalIndex.java
@@ -0,0 +1,60 @@
+package timeBench.data.util;
+
+import prefuse.util.collections.IntIterator;
+
+
+/**
+ * Represents an index over two column of data, allowing quick lookups of
+ * ranges that lie within a given data value.
+ * 
+ * @author bilal
+ */
+public interface IntervalIndex {
+
+    /**
+     * Perform an initial indexing of a data column.
+     */
+    public void index();
+    
+    /**
+     * Get the comparator used to compare column data values.
+     * @return the sort comparator
+     */
+    public IntervalComparator getComparator();
+    
+    /**
+     * Get the row (or one of the rows) with the minimum data value.
+     * @return a row with a minimum data value
+     */
+    public int minimum();
+    
+    /**
+     * Get the row (or one of the rows) with the maximum data value.
+     * @return a row with a maximum data value
+     */
+    public int maximum();
+    
+    /**
+     * Get the size of this index, the number of data value / row
+     * pairs included.
+     * @return the size of the index
+     */
+    public int size();
+    
+
+    /**
+     * Get an iterator over a range of rows.
+     * @param lo the minimum data value
+     * @param hi the maximum data value
+     * @return an iterator over a range of rows
+     */
+    public IntIterator rows(long lo, long hi);
+    
+    /**
+     * Get an iterator over all rows whose ranges contain the given data value.
+     * @param val the data value
+     * @return an iterator over all rows matching the data value
+     */
+    public IntIterator rows(long val);
+    
+}
---
+++ src/timeBench/test/TestIntervalSearch.java
@@ -0,0 +1,41 @@
+package timeBench.test;
+
+import prefuse.data.Table;
+import prefuse.util.collections.IntIterator;
+import timeBench.data.util.DefaultIntervalComparator;
+import timeBench.data.util.IntervalComparator;
+import timeBench.data.util.IntervalTreeIndex;
+
+public class TestIntervalSearch {
+	/**
+	 * Test
+	 * 
+	 * @param args
+	 *            system arguments
+	 */
+	public static void main(String[] args) {
+		Table table = new Table(10, 2);
+		table.addColumn("start", long.class);
+		table.addColumn("end", long.class);
+		table.set(0, 0, 10);table.set(0, 1, 20);
+		table.set(1, 0, 14);table.set(1, 1, 25);
+		table.set(2, 0, 10);table.set(2, 1, 16);
+		table.set(3, 0, 16);table.set(3, 1, 18);
+		table.set(4, 0, 21);table.set(4, 1, 26);
+		table.set(5, 0, 16);table.set(5, 1, 26);
+		table.set(6, 0, 8); table.set(6, 1, 12);
+		table.set(7, 0, 18);table.set(7, 1, 22);
+		table.set(8, 0, 22);table.set(8, 1, 26);
+		table.set(9, 0, 10);table.set(9, 1, 12);
+
+		IntervalTreeIndex intervalTree = new IntervalTreeIndex(table, table.rows(),
+				table.getColumn(0), table.getColumn(1),
+				new DefaultIntervalComparator());
+		IntIterator result = intervalTree.rows(13, 19);
+		while (result.hasNext()) {
+			System.out.println(result.nextInt());
+		}
+	}
+
+
+}
