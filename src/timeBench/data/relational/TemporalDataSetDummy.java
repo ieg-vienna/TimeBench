3b8d7f75619f6277a58007f62f7303f8ce9fc9bb
Alexander Rind <rind@ifs.tuwien.ac.at>
2011-09-07 13:21:06 +0000

    helper infrastructure for temp.d.s. reading

---
+++ src/timeBench/data/relational/TemporalDataSetDummy.java
@@ -0,0 +1,10 @@
+package timeBench.data.relational;
+
+/**
+ * temporary placeholder while temporal dataset does not exist
+ * 
+ * @author Alexander Rind
+ */
+public class TemporalDataSetDummy {
+
+}
