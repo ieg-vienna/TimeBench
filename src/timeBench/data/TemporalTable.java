764ecae1a7f248942b706feae25eb8e793eb15bc
Alexander Rind <rind@ifs.tuwien.ac.at>
2013-04-18 13:20:31 +0000

    expression hidden as inner class of TemporalTable; simplified; javadoc

--- src/timeBench/data/TemporalTable.java
+++ src/timeBench/data/TemporalTable.java
@@ -1,11 +1,23 @@
 package timeBench.data;
 
 import prefuse.data.DataTypeException;
+import prefuse.data.Graph;
+import prefuse.data.Schema;
 import prefuse.data.Table;
+import prefuse.data.Tuple;
 import prefuse.data.column.Column;
 import prefuse.data.column.ExpressionColumn;
-import timeBench.data.expression.TemporalColumnExpression;
+import prefuse.data.expression.ColumnExpression;
+import prefuse.data.expression.ExpressionVisitor;
+import timeBench.data.expression.TemporalExpression;
 
+/**
+ * Minimal prefuse data structure for time-oriented data. This is internally
+ * used by {@link TemporalDataset} and can be applied to build arbitrary data
+ * structures. E.g., {@link Graph#Graph(Table, Table, boolean)}
+ * 
+ * @author Rind
+ */
 public class TemporalTable extends Table {
     
     public static final String ID_POSTFIX = "_id";
@@ -29,21 +41,35 @@ public class TemporalTable extends Table {
         return super.removeColumn(idx);
     }
     
+    /**
+     * Yields the name of the column storing the {@link TemporalElement#ID} for
+     * a given TemporalColumn.
+     * 
+     * @param name
+     *            column name for {@link TemporalElement}
+     * @return column name for id
+     */
     public static String idColumnNameFor(String name) {
         return name + ID_POSTFIX;
     }
 
+    /**
+     * Specialized column that allows temporal elements to be retrieved by
+     * {@link TemporalPrimitveByIdColumnExpression} and in addition be saved.
+     * 
+     * @author Rind
+     */
     static class TemporalColumn extends ExpressionColumn {
         
         // Alternatively it would be possible to extend AbstractColumn directly
         // but in that case the caching would need to be replicated
         
-        Table table;
-        TemporalElementStore store;
-        String idColumn;
+        private Table table;
+        private TemporalElementStore store;
+        private String idColumn;
 
         public TemporalColumn(Table table, String idColumn, TemporalElementStore store) {
-            super(table, new TemporalColumnExpression(idColumn, store, true));
+            super(table, new TemporalPrimitveByIdColumnExpression(idColumn, store));
             this.table = table;
             this.store = store;
             this.idColumn = idColumn;
@@ -93,5 +119,55 @@ public class TemporalTable extends Table {
 //            }
 //            return super.getString(row);
 //        }
+    }
+    
+    /**
+     * Yields a temporal primitive based on its {@link TemporalElement#ID} from
+     * another column.
+     * 
+     * @author Rind
+     */
+    static class TemporalPrimitveByIdColumnExpression extends TemporalExpression {
+
+        // Alternatively it could accept id as expression (e.g., [field]+1)?
+
+        protected final String field;
+        protected final TemporalElementStore store;
+
+        public TemporalPrimitveByIdColumnExpression(String field,
+                TemporalElementStore store) {
+            this.field = field;
+            this.store = store;
+        }
+
+        // TODO should this extend TemporalExpression? in that case getType() inherited
+        @SuppressWarnings("rawtypes")
+        @Override
+        public Class getType(Schema s) {
+            return TemporalElement.class;
+        }
+
+        @Override
+        public Object get(Tuple t) {
+            long id = t.getLong(this.field);
+            return store.getTemporalPrimitive(id);
+            // store.getTemporalElement(id);
+        }
+
+        public String getField() {
+            return field;
+        }
+
+        public TemporalElementStore getTemporalElementStore() {
+            return store;
+        }
+
+        @Override
+        public void visit(ExpressionVisitor v) {
+            super.visit(v);
+            // expose id column to ExpressionAnalyzer.getReferencedColumns()
+            ColumnExpression c = new ColumnExpression(field);
+            v.down(); c.visit(v); v.up();
+        }
     }
 }
